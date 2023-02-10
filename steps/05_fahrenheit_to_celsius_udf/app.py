bd74e2c9ef46ae5c3c01d7a9448ebfc185719611
Scott McGougan <scott.mcgougan@snowflake.com>
2023-02-09 21:25:22 -0600

    udf changes pt 2

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -13,7 +13,7 @@ import sys
 from scipy.constants import convert_temperature
 
 def main(temp_f: float) -> float:
-    return convert_temperature(float(temp_f), 'F', 'C')
+    return  convert_temperature(float(temp_f), 'F', 'C')
 
 
 # For local debugging
